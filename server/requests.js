2ce24cd33788a24a37b0a7a64985a66f4848c618
SmartForger <23446619+SmartForger@users.noreply.github.com>
2019-04-10 14:16:14 -0700

    update server for performance

--- server/requests.js
+++ server/requests.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const moment = require("moment");
 
 const BASE_URL = "https://api.webstorage-service.com/v1";
 let channels = [];
@@ -20,68 +21,91 @@ axios.interceptors.request.use(
   error => Promise.reject(error)
 );
 
+let channelTimer = null;
 function getChannels() {
-  axios
-    .post(`${BASE_URL}/devices/current`, {
-      "api-key": "1berqh6otk5rcupdb8s9mr9db238mo0kplrgcamgm53ff",
-      "login-id": "rdga9751",
-      "login-pass": "eifjN23S"
-    })
-    .then(res => {
-      channels = res.data.devices || [];
+  if (channelTimer) {
+    clearInterval(channelTimer);
+  }
 
-      const resetLimit = +res.headers["x-ratelimit-reset"] || 120;
-      const limitCount = +res.headers["x-ratelimit-limit"] || 10;
-      const interval = resetLimit / limitCount;
+  channelTimer = setTimeout(() => {
+    axios
+      .post(`${BASE_URL}/devices/current`, {
+        "api-key": "1berqh6otk5rcupdb8s9mr9db238mo0kplrgcamgm53ff",
+        "login-id": "rdga9751",
+        "login-pass": "eifjN23S"
+      })
+      .then(res => {
+        channels = res.data.devices || [];
 
-      setTimeout(getChannels, interval * 1000);
+        // const resetLimit = +res.headers["x-ratelimit-reset"] || 120;
+        // const limitCount = +res.headers["x-ratelimit-limit"] || 10;
+        // const interval = resetLimit / limitCount;
 
-      if (sockets) {
-        sockets.emit("channels", channels);
-      }
+        // setTimeout(getChannels, interval * 1000);
 
-      console.log(`fetched channels. next fetch in ${interval} seconds`);
-    });
+        if (sockets) {
+          sockets.emit("channels", channels);
+        }
+
+        console.log(
+          `fetched channels at ${moment().format("MMM DD, YYYY kk:mm:ss")}`
+        );
+      });
+  }, 120000);
 }
 
 function getStoredChannels() {
+  if (channels.length === 0) {
+    getChannels();
+  }
+
   return channels;
 }
 
+let dataTimer = null;
 function getLatestData() {
-  Promise.all(
-    serials.map(serial =>
-      axios
-        .post(`${BASE_URL}/devices/latest-data`, {
-          "api-key": "1berqh6otk5rcupdb8s9mr9db238mo0kplrgcamgm53ff",
-          "login-id": "rdga9751",
-          "login-pass": "eifjN23S",
-          "remote-serial": serial
-        })
-        .then(res => {
-          data[serial] = res.data || [];
-          sockets.emit(`ch:${serial}`, res.data);
+  if (dataTimer) {
+    clearInterval(dataTimer);
+  }
 
-          const resetLimit = +res.headers["x-ratelimit-reset"] || 60;
-          const limitCount = +res.headers["x-ratelimit-limit"] || 60;
+  dataTimer = setInterval(() => {
+    Promise.all(
+      serials.map(serial =>
+        axios
+          .post(`${BASE_URL}/devices/latest-data`, {
+            "api-key": "1berqh6otk5rcupdb8s9mr9db238mo0kplrgcamgm53ff",
+            "login-id": "rdga9751",
+            "login-pass": "eifjN23S",
+            "remote-serial": serial
+          })
+          .then(res => {
+            data[serial] = res.data || [];
+            sockets.emit(`ch:${serial}`, res.data);
 
-          return resetLimit / limitCount;
-        })
-    )
-  ).then(intervals => {
-    if (intervals.length > 0) {
-      let max = 0;
-      intervals.forEach(i => {
-        max = Math.max(i, max);
-      });
-      setTimeout(getLatestData, max * intervals.length * 1000);
+            const resetLimit = +res.headers["x-ratelimit-reset"] || 60;
+            const limitCount = +res.headers["x-ratelimit-limit"] || 60;
+
+            return resetLimit / limitCount;
+          })
+      )
+    ).then(intervals => {
+      // if (intervals.length > 0) {
+      //   let max = 0;
+      //   intervals.forEach(i => {
+      //     max = Math.max(i, max);
+      //   });
+      //   setTimeout(getLatestData, max * intervals.length * 1000);
+      //   console.log(
+      //     `fetched latest data, next fetch in ${max * intervals.length} seconds`
+      //   );
+      // } else {
+      //   setTimeout(getLatestData, 1000);
+      // }
       console.log(
-        `fetched latest data, next fetch in ${max * intervals.length} seconds`
+        `data fetched at ${moment().format("MMM DD, YYYY kk:mm:ss")}`
       );
-    } else {
-      setTimeout(getLatestData, 1000);
-    }
-  });
+    });
+  }, 120000);
 }
 
 function setSocketIo(io) {
@@ -90,6 +114,7 @@ function setSocketIo(io) {
 
 function setSerials(list) {
   serials = list;
+  getLatestData();
 }
 
 function getStoredData() {
