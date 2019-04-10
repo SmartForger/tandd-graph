e62c8e01a11ab9c3cd837776b8c24f737a392eb0
SmartForger <23446619+SmartForger@users.noreply.github.com>
2019-04-10 13:28:52 -0700

    enable ssl

--- server/index.js
+++ server/index.js
@@ -12,16 +12,16 @@ const {
 } = require("./requests");
 const initSocketServer = require("./socket");
 
-// const forceSSL = function() {
-//   return function(req, res, next) {
-//     if (req.headers["x-forwarded-proto"] !== "https") {
-//       return res.redirect(["https://", req.get("Host"), req.url].join(""));
-//     }
-//     next();
-//   };
-// };
+const forceSSL = function() {
+  return function(req, res, next) {
+    if (req.headers["x-forwarded-proto"] !== "https") {
+      return res.redirect(["https://", req.get("Host"), req.url].join(""));
+    }
+    next();
+  };
+};
 
-// app.use(forceSSL());
+app.use(forceSSL());
 app.use(express.static(path.join(__dirname, "../dist")));
 
 app.get("/api/channels", (req, res) => {
